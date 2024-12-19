e8c7db7bb8255ac227dc86e973551748eaa68328
justcoom <vbruner11@mail.ru>
2024-12-19 23:39:16 +0700

    add theme and global styles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,44 @@
+import styled, {createGlobalStyle} from "styled-components";
+import {theme} from "./Theme";
+
+export const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    body {
+        margin: 0;
+        font-family: "Epilogue", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: ${theme.colors.font};
+    }
+    
+    a {
+        text-decoration: none;
+    }
+    
+    ul {
+        list-style: none;
+    }
+    
+    button {
+        background: unset;
+        border: none;
+    }
+    
+    section:nth-of-type(odd) {
+        background-color: ${theme.colors.primaryBg};
+    }
+    
+    section:nth-of-type(even) {
+        background-color: ${theme.colors.secondaryBg};
+    }
+    
+`
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,10 @@
+
+export const theme = {
+    colors: {
+        primaryBg: "rgba(255, 255, 255, 1)",
+        secondaryBg: "rgba(19, 18, 18, 1)",
+        accent: "#ffd700",
+
+        font: "#FFF"
+    }
+}
