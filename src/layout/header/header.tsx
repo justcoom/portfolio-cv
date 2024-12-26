d19b70b1d382caa00851adac5f94638e62d1d4eb
justcoom <vbruner11@mail.ru>
2024-12-26 21:58:04 +0700

    add header styles

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -2,20 +2,32 @@ import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/logo";
 import {Menu} from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
 
 const items = ["Projects", "About", "Digital Assets"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify="space-between" align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    background-color: bisque;
+    background-color: rgba(255, 255, 255, 1);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99999;
+    padding: 20px 0;
     display: flex;
     justify-content: space-between;
 `
