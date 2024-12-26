45b4066dc339f4be0505cc430807e689189fe2a5
justcoom <vbruner11@mail.ru>
2024-12-26 21:55:01 +0700

    container for all page

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    // 1440 - 52 - 52 + 15 + 15 (падинги) хотя говорят максимум 1200px
+    max-width: 1070px;
+    width: 100%;
+    min-height: 100%;
+    padding: 0 15px;
+    margin: 0 auto;
+    outline: 1px solid red;
+`
