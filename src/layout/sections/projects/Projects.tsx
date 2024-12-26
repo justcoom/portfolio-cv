795e99bc2b63b9aae525ddf223f53f8f5cb5de73
justcoom <vbruner11@mail.ru>
2024-12-26 21:57:21 +0700

    add project styles

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -6,29 +6,36 @@ import {Project} from "./project/Project";
 import projImg from "../../../assets/images/proj-1.webp"
 import projImg2 from "../../../assets/images/proj-2.webp"
 import projImg3 from "../../../assets/images/proj-3.webp"
+import {Container} from "../../../components/Container";
 
 // const ProjectItems = ["32", "423", "dfssdfs", "fsdfsd"]
 
 export const Projects = () => {
     return (
         <ProjectBlock>
-            <ProjectsTitle>MY PROJECTS</ProjectsTitle>
-            <FlexWrapper direction={"column"} gap={"78px"} align={"center"}>
-                <Project projectType={"Mental Health"} title={"A Case Study_ Find Peace in Meditation"}
-                         text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."} img={projImg}/>
-                <Project projectType={"Fashion Show"} title={"A Case Study_ Enjoy Fashion Shows From Home"}
-                         text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."} img={projImg2}/>
-                <Project projectType={"Barbeque"} title={"Branding_ Chicken Barbeque Never Goes Wrong"}
-                         text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."} img={projImg3}/>
-            </FlexWrapper>
+            <Container>
+                <ProjectsTitle>MY PROJECTS</ProjectsTitle>
+                <FlexWrapper direction={"column"} gap={"78px"} align={"center"}>
+                    <Project projectType={"Mental Health"} title={"A Case Study_ Find Peace in Meditation"}
+                             text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."} img={projImg}/>
+                    <Project projectType={"Fashion Show"} title={"A Case Study_ Enjoy Fashion Shows From Home"}
+                             text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."} img={projImg2}/>
+                    <Project projectType={"Barbeque"} title={"Branding_ Chicken Barbeque Never Goes Wrong"}
+                             text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."} img={projImg3}/>
+                </FlexWrapper>
+            </Container>
         </ProjectBlock>
     );
 };
 
 const ProjectBlock = styled.section`
-    background-color: lightgreen;
-    min-height: 100vh;
+    padding: 170px 0;
 `
 const ProjectsTitle = styled.h2`
-    text-align: center;
+    color: rgba(45, 44, 44, 1);
+    font-size: 120px;
+    font-weight: 800;
+    line-height: 0.935;
+    max-width: 772px;
+    margin-bottom: 70px;
 `
--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../../components/icon/Icon";
+import {theme} from '../../../../styles/Theme';
+import {FlexWrapper} from "../../../../components/FlexWrapper";
 
 type ProjectPropsType = {
     projectType?: string;
@@ -12,13 +14,19 @@ type ProjectPropsType = {
 export const Project = (props: ProjectPropsType) => {
     return (
         <StyledProject img={props.img}>
-            <ProjectType>{props.projectType}</ProjectType>
-            <ProjectTitle>{props.title}</ProjectTitle>
-            <ProjectText>{props.text}</ProjectText>
-            <Link href="#">
-                View More
-                <Icon iconId={"arrow-right"} height={"16"} viewBox={"0 0 16 16"} width={"16"}></Icon>
-            </Link>
+            <FlexWrapper gap={"80px"}>
+                <TitleWrapper>
+                    <ProjectType>{props.projectType}</ProjectType>
+                    <ProjectTitle>{props.title}</ProjectTitle>
+                </TitleWrapper>
+                <TextWrapper>
+                    <ProjectText>{props.text}</ProjectText>
+                    <Link href="#">
+                        View More
+                        <Icon iconId={"arrow-right"} height={"16"} viewBox={"0 0 16 16"} width={"16"}></Icon>
+                    </Link>
+                </TextWrapper>
+            </FlexWrapper>
         </StyledProject>
     );
 };
@@ -28,22 +36,48 @@ const StyledProject = styled.div<ProjectPropsType>`
     background-blend-mode: multiply;
 
     max-width: 1040px;
-    height: 447px;
+    min-height: 447px;
     object-fit: cover;
+    padding: 92px 46px 100px;
 
+`
+
+const TitleWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    
+    max-width: 433px;
+`
+
+const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    
+    max-width: 433px;
 `
 
 const ProjectType = styled.span`
-    color: rgba(255, 255, 255, 1);
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 1.5;
 `
 
 const ProjectTitle = styled.h3`
-    color: rgba(255, 255, 255, 1);
+    font-weight: 800;
+    font-size: 45px;
 `
 
 const ProjectText = styled.p`
-    color: rgba(255, 255, 255, 1);
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 1.5;
+    margin-top: 40px;
 `
 const Link = styled.a`
-    color: rgba(255, 255, 255, 1);
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${theme.colors.font}
 `
